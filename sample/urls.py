bbd7fea03a50cd6609aa20ec67c5b062f27f30b4
Samuel Bowman <samuelpbowman@gmail.com>
2019-09-25 19:58:38 -0400

    fixed style issues

--- sample/urls.py
+++ sample/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('number', views.give_me_a_number, name='random number')
+    path('number', views.give_me_a_number, name='number')
 ]
